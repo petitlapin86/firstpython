3bd8843c28899e9100614df7442e4a79f87f7724
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-15 14:49:24 -0400

    continue lists

--- list.py
+++ list.py
@@ -8,5 +8,5 @@ language = ["python", "Javascript", "PHP", "Ruby"]
 #.append adds a list item
 #del remove a list item - its really just deleting the label not the object
 #.pop removes the last item of the list
+#.copy makes a copy of the list 
 #indexing list items starts at zero
-  
