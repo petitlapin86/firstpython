8e408aa621528ce121e4abc4178159d0bade4b11
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-15 11:14:51 -0400

    figured out error! --updated python version

--- python.py
+++ python.py
@@ -6,15 +6,15 @@ tickets_remaining = 100
 print("there are {} tickets remaining.".format(tickets_remaining))
 
 #gather the users name and assign it to a new variable
-name = input("whats your name? ")
+name = input("What is your name: ")
 
 #prompt the user by name and ask how many tickets they would like
-number_of_tickets = ("hey, {} how many tickets would you like? ".format(name))
+num_tickets = input("How many tickets would you like, {}? ".format(name))
 
-number_of_tickets = int(number_of_tickets) #reassign variable to number of tickets
+num_tickets = int(num_tickets) #reassign variable to number of tickets
 
 #calculate the price (number of tickets multiplied by the price) and assign it to a variable
-amount_due = number_of_tickets * TICKET_PRICE
+amount_due = num_tickets * TICKET_PRICE
 
 #output the total
-print("the total due is ${}".format(amount_due))
+print("That will cost you {}".format(amount_due))
