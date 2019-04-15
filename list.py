c48085593cc805e2101361154c4f7e301de3dd84
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-15 14:07:04 -0400

    working on list concepts

--- list.py
+++ list.py
@@ -1,1 +1,9 @@
-#Grocery List 
+#Grocery List
+
+#this is a list
+#lists are mutable
+language = ["python", "Javascript", "PHP", "Ruby"]
+
+#extend adds a list on to a list
+#append adds a list item
+#indexing list items starts at zero
