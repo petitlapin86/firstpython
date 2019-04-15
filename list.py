bf6579fb185a6b13d1fbc598e85c61cfd6329060
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-15 14:30:23 -0400

    list item techniques

--- list.py
+++ list.py
@@ -4,6 +4,9 @@
 #lists are mutable
 language = ["python", "Javascript", "PHP", "Ruby"]
 
-#extend adds a list on to a list
-#append adds a list item
+#.extend adds a list on to a list
+#.append adds a list item
+#del remove a list item - its really just deleting the label not the object
+#.pop removes the last item of the list
 #indexing list items starts at zero
+  
