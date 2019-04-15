3aa3df2b1f995256b85d66de55d6283a180106df
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-15 11:53:50 -0400

    write first while block

--- python.py
+++ python.py
@@ -2,30 +2,37 @@ TICKET_PRICE = 10
 
 tickets_remaining = 100
 
-#output how many tickets are remaining using the tickets_remaining variable
-print("there are {} tickets remaining.".format(tickets_remaining))
 
-#gather the users name and assign it to a new variable
-name = input("What is your name: ")
 
-#prompt the user by name and ask how many tickets they would like
-num_tickets = input("How many tickets would you like, {}? ".format(name))
+#run this code continuously until we run out of tickets_remaining
+while tickets_remaining:
 
-num_tickets = int(num_tickets) #reassign variable to number of tickets
+    #output how many tickets are remaining using the tickets_remaining variable
+    print("there are {} tickets remaining.".format(tickets_remaining))
 
-#calculate the price (number of tickets multiplied by the price) and assign it to a variable
-amount_due = num_tickets * TICKET_PRICE
+    #gather the users name and assign it to a new variable
+    name = input("What is your name: ")
 
-#output the total to the screen
-print("That will cost you {}".format(amount_due))
+    #prompt the user by name and ask how many tickets they would like
+    num_tickets = input("How many tickets would you like, {}? ".format(name))
 
-#as a user i should be able to confirm my order Y/N?
-should_proceed = input("Would you like to purchase now? Type Y or N to proceed  ")
-if should_proceed.lower() == "y":
+    num_tickets = int(num_tickets) #reassign variable to number of tickets
+
+    #calculate the price (number of tickets multiplied by the price) and assign it to a variable
+    amount_due = num_tickets * TICKET_PRICE
+
+    #output the total to the screen
+    print("That will cost you {}".format(amount_due))
+
+    #as a user i should be able to confirm my order Y/N?
+    should_proceed = input("Would you like to purchase now? Type Y or N to proceed  ")
+    if should_proceed.lower() == "y":
         print("SOLD!")
-        #todo : gather credit card info and process it here.
-        tickets_remaining -= num_tickets #updating tickets remaining
+    #todo : gather credit card info and process it here.
+        tickets_remaining -= num_tickets ##decrement tickets_remaining
 
-else: print("Thank you anyways, {} have a good day!".format(name))
+    else: print("Thank you anyways, {} have a good day!".format(name))
 
-            #decrement tickets_remaining
+
+#notify user that tickets are sold out
+print("sorry the tickets are all sold out")
