5f59c537e2d20a8bc7d6b0e26fe54363ede035c5
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-16 10:17:25 -0400

    input indexing

--- collections.py
+++ collections.py
@@ -1,57 +1,83 @@
 import os
 
-shopping_list = [] #empty list or array
+shopping_list = []
+
 
 def clear_screen():
-    os.system("cls" if os.name == "nt" else "clear")
+  os.system("cls" if os.name == "nt" else "clear")
+
 
 def show_help():
     clear_screen()
     print("What should we pick up at the store?")
     print("""
-    Enter 'DONE' to stop adding items.
-    Enter 'HELP' for this help
-    Enter 'SHOW' to show shopping list
-    """)
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
+Enter 'REMOVE' to delete an item from your list.
+""")
 
-def add_to_list(item): #function that appends items to shopping list
+
+def add_to_list(item):
     show_list()
     if len(shopping_list):
-        position = input("where should I add {}?\n")
-                         "Press ENTER to add to the end of the list\n")
-    shopping_list.append(new_item)
-    #notify user that item was added state total number of items in lists
-    print("Added! Your list has {} items total.".format(len(shopping_list)))
+        position = input("Where should I add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
+    else:
+        position = 0
+
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+
+    show_list()
 
 
 def show_list():
     clear_screen()
 
-    print("Heres your list:")
+    print("Here's your list:")
 
-    index = 1 #starting list at index one instead of zero
-
+    index = 1
     for item in shopping_list:
-        print("{}".format(index, item))
-        index += 1 #incrementing index by one for each new item
+        print("{}. {}".format(index, item))
+        index += 1
 
-        print("-"*10) #aesthetically pleasing divider
+    print("-"*10)
 
 
-show_help() #begin program by printing show help to screen
+def remove_from_list():
+    show_list()
+    what_to_remove = input("What would you like to remove?\n> ")
+    try:
+        shopping_list.remove(what_to_remove)
+    except ValueError:
+        pass
+    show_list()
 
-while True: #infinite loop
+
+show_help()
+
+while True:
     new_item = input("> ")
 
     if new_item.upper() == 'DONE' or new_item.upper() == 'QUIT':
-        break #break out of loop with input done
+        break
     elif new_item.upper() == 'HELP':
         show_help()
         continue
-    elif new_item.upper == 'show':
+    elif new_item.upper() == 'SHOW':
         show_list()
         continue
+    elif new_item.upper() == 'REMOVE':
+        remove_from_list()
     else:
-    add_to_list(new_item)
+        add_to_list(new_item)
 
 show_list()
