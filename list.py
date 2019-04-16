3b2d6fdaa0f9d58303947c1ced3747c0a6f2bb57
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-16 08:23:06 -0400

    begin grocery list example in py

--- list.py
+++ list.py
@@ -1,12 +1,31 @@
 #Grocery List
 
-#this is a list
+#this is a list: language = ["python", "Javascript", "PHP", "Ruby"]
 #lists are mutable
-language = ["python", "Javascript", "PHP", "Ruby"]
 
 #.extend adds a list on to a list
 #.append adds a list item
 #del remove a list item - its really just deleting the label not the object
 #.pop removes the last item of the list
-#.copy makes a copy of the list 
+#.copy makes a copy of the list
+#.slice
+#.split Split will take a string and create elements based on a delimiter
+#.join takes an iterable and turns it into a string
 #indexing list items starts at zero
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+    Enter 'DONE' to stop adding items.
+    Enter 'HELP' for this help
+    """)
+
+show_help()
+
+ while True: #infinite loop
+     new_item = input("> ")
+
+     if new_item == 'DONE':
+         break #break out of loop with input done
+     elif new_item == 'HELP':
+         show_help()
