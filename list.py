4d369a0315c7e28605621861f0c4cdf5df6d6599
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-16 08:51:58 -0400

    using break and continue

--- list.py
+++ list.py
@@ -13,19 +13,41 @@
 #.join takes an iterable and turns it into a string
 #indexing list items starts at zero
 
+shopping_list = [] #empty list or array
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
     Enter 'DONE' to stop adding items.
     Enter 'HELP' for this help
+    Enter 'SHOW' to show shopping list
     """)
 
-show_help()
+def add_to_list(item): #function that appends items to shopping list
+    shopping_list.append(item)
+    #notify user that item was added state total number of items in lists
+    print("Added! Your list has {} items total.".format(len(shopping_list)))
 
- while True: #infinite loop
-     new_item = input("> ")
+def show_list():
+    print("Heres your list:")
+    for item in shopping_list:
+        print(item)
 
-     if new_item == 'DONE':
-         break #break out of loop with input done
-     elif new_item == 'HELP':
-         show_help()
+
+show_help() #begin program by printing show help to screen
+
+while True: #infinite loop
+    new_item = input("> ")
+
+    if new_item == 'DONE':
+        break #break out of loop with input done
+    elif new_item == 'HELP':
+        show_help()
+        continue
+    elif new_item == 'show':
+        show_list()
+        continue
+
+    add_to_list(new_item)
+
+show_list()
