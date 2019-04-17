eaada951a6d8349a4bd69c1bce9a50ff24097844
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-17 19:59:13 -0400

    learning about dictionaries

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#dictionary
+
+#dictionaries are similar to lists however they arent sorted
+#each dictionary has a key and a value
+
+course = {"title": "learn python"}
+
+#title is the key
+#learn python is the value
