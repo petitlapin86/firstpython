a68d3b8aa46318be96565f64060ec242eb1a981d
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-23 17:45:27 -0400

    set collections

--- sets.py
+++ sets.py
@@ -17,3 +17,25 @@ numbers.remove(4)
 
 #to discard (which tries to remove the value, but if it doesnt exist just moves on)
 numbers.discard(6)
+
+#heres an example
+#you have two sets
+set1 = {1,2,3,4,5}
+set2 = {6,7,8,9,10}
+
+set1.union(set2)
+#union will return
+{1,2,3,4,5,6,7,8,9,10}
+
+
+set1.difference(set2)
+#difference will return
+{set(}1,2,3,4,5}
+
+set1.symmetric_difference(set2)
+#symmetric_difference returns
+{1,2,3,4,5,6,7,8,9,10}
+
+set1.intersection(set2)
+#intersection returns
+set()
