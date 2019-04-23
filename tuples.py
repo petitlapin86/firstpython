f0c36e287bb07c23016dabe204f44a053b04b7af
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-23 16:36:30 -0400

    tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#tuples
+
+#this is a tuple
+#tuples are like lists in that they have index numbers
+#tuples are imutable (cant be changed in place)
+#tuples are often shown with parenthesis
+#but technically its the commas that make up a tuple
+
+my_tuple = (1, 2, 3)
+my_second_tuple = 1, 2, 3
+my_third_tuple = (5,)
