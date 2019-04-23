f9b884ef90f007ea9409476e8e8fb4310a55f679
petitlapin86 <skinnerpa86@yahoo.com>
2019-04-23 12:48:12 -0400

    packing and unpacking

--- dictionary.py
+++ dictionary.py
@@ -7,3 +7,21 @@ course = {"title": "learn python"}
 
 #title is the key
 #learn python is the value
+
+
+#packing explained:
+#takes a bunch of key value pairs and turns them into a dictionaries keys and values
+#most useful in functions
+def packer(**kwargs): #kwargs is 'keyword arguments'
+    print(kwargs)
+
+packer(name="kenneth", num=42, spanish_inquisition=none)
+
+
+#unpacking
+
+def unpacker(first_name=None, last_name=None):
+    if first_name and last_name:
+        print("hi {} {}!".format(first_name, last_name))
+    else:
+        print("Hi, no name!")
