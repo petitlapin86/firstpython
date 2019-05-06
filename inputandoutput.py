b8313c69de69122f2d0ae11b0b91f4bba8b36cc5
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-06 17:41:33 -0400

    finish mad lib challenge

--- inputandoutput.py
+++ inputandoutput.py
@@ -1,15 +1,15 @@
 #input and output
 
 #ask for users name
-print("what is your name? ")
+name = input("what is your name? ")
 
 #welcome user with name
 print("Hi {}, get ready to play madlibs!".format(name))
 
-verb = input("Please enter a verb:  \n")
-noun = input("Please enter a noun:  \n")
-adjective = input("Please enter a adjective:  \n")
+verb = input("Please enter a verb:  ")
+noun = input("Please enter a noun:  ")
+adjective = input("Please enter a adjective:  ")
 
-print("I enjoy running. I find it helps me to {} better. \n
-        without running, my {} would probably not even work.
-        My speed is getting more {} every single day!".format(verb, noun, adjective))
+print("I enjoy running. I find it helps me to", verb, "better.")
+print("without running, my", noun, "would probably not even work.")
+print("My speed is getting more", adjective, "every single day!")
