ab65ab78e11c5b1a4b2d5fb3347ec5ecb4199a44
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-06 17:28:59 -0400

    while loop example

--- looping.py
+++ looping.py
@@ -1,1 +1,16 @@
-loops
+#loops
+
+#ask user for name
+name = input("What is your name? ")
+
+#ask the user by name if they understand while loops
+understanding = input("{}, do you understand python while loops?\n (Enter yes/no)".format(name))
+
+#check if the user doesnt understand while loops. also converts input to lowercase so YES == yes
+while understanding.lower() != 'yes':
+    #since the user doesnt understand while loops lets explain
+    print("Okay, {}, while loops in python repeat as long as a certain boolean condition is met.".format(name))
+
+    understanding = input("{}, now do you understand python while loops?\n (Enter yes/no)".format(name))
+
+print("Thats great {}. I am pleased that you understand while loops now, that was getting repetitive.".format(name))
