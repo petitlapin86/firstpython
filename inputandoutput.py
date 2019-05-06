75f48d2f143d8d0d964f36899e08ee3b3154bbe4
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-06 17:34:17 -0400

    begin madlib challenge

--- inputandoutput.py
+++ inputandoutput.py
@@ -1,1 +1,7 @@
-#input and output 
+#input and output
+
+print("what is your name? ")
+
+print("I enjoy running. I find it helps me to {} better. \n
+        without running, my {} would probably not even work.
+        My speed is getting more {} every single day!".format(verb, noun, adjective))
