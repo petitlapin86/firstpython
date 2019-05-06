6e289da4b84d48e0c734e171948eaedfa0f9c831
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-06 18:39:23 -0400

    handy functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+#helpful functions
+#these are built in functions that you can use in python with some example of how to use them
+
+isinstance('a', str) #is instance 'a' a string?
+
+isinstance(5.2, (int, float)) #is instance 5.2 a int or a float?
+
+
+issubclass(Thief, Character) #tells you whether its a subclass of another class - so here im asking is theif a subclass of character?
