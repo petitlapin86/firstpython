e30ccd1a43f6dd032fd5f5cc06b13decf79cf841
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-06 17:37:50 -0400

    using input prompt

--- inputandoutput.py
+++ inputandoutput.py
@@ -1,6 +1,14 @@
 #input and output
 
+#ask for users name
 print("what is your name? ")
+
+#welcome user with name
+print("Hi {}, get ready to play madlibs!".format(name))
+
+verb = input("Please enter a verb:  \n")
+noun = input("Please enter a noun:  \n")
+adjective = input("Please enter a adjective:  \n")
 
 print("I enjoy running. I find it helps me to {} better. \n
         without running, my {} would probably not even work.
